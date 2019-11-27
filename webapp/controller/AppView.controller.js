1632e6b44becd5d6f4283d8762d4fc2779b07f0e
Nils Lutz <info@nilslutz.de>
2019-11-27 15:44:35 +0100

    feat: Remove mock data and connect graph to blockchain

--- webapp/controller/AppView.controller.js
+++ webapp/controller/AppView.controller.js
@@ -1,108 +1,139 @@
-sap.ui.define(
-  [
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel",
-    "sap/suite/ui/microchart/ComparisonMicroChart",
-    "sap/suite/ui/microchart/ComparisonMicroChartData"
-  ],
-  function(
-    Controller,
-    JSONModel,
-    ComparisonMicroChart,
-    ComparisonMicroChartData
-  ) {
-    "use strict";
+sap.ui.define(['sap/ui/core/mvc/Controller', 'sap/ui/model/json/JSONModel'], function(Controller, JSONModel) {
+    'use strict';
 
-    return Controller.extend(
-      "io.dev.foodchain.ui5-producer-app.controller.AppView",
-      {
+    return Controller.extend('io.dev.foodchain.ui5-producer-app.controller.AppView', {
         onInit: function() {
-          var oModel = new JSONModel(
-            jQuery.sap.getModulePath(
-              "io.dev.foodchain.ui5-producer-app.model",
-              "/graph.json"
-            )
-          );
-          var that = this;
+            var that = this;
+            var oModel = new JSONModel();
+            var aNodesArray = [];
+            var aLinesArray = [];
+            // get BatchNetwork with id 1
+            $.ajax({
+                url: 'http://localhost:3000/api/BatchNetwork/1',
+                dataType: 'JSON',
+                async: false,
+            }).done(function(oBatchNetwork) {
+                // iterate over all Batch nodes and request detail information from REST API
+                oBatchNetwork.nodes.forEach(oBatch => {
+                    $.ajax({
+                        url: 'http://localhost:3000/api/Batch/' + oBatch.split('#').pop(),
+                        dataType: 'JSON',
+                        async: false,
+                    }).done(function(oBatch) {
+                        // transform requested data to fit UI5 control model
+                        aNodesArray.push({
+                            key: oBatch.batchId,
+                            title: 'Batch_' + oBatch.batchId,
+                            icon: 'sap-icon://database',
+                            status: Number(oBatch.batchId) < 3 ? 'Warning' : 'Success',
+                            group: Number(oBatch.batchId) < 3 ? 'F' : 'S',
+                            shape: 'Box',
+                            maxWidth: 600,
+                            attributes: [
+                                {
+                                    label: 'Description',
+                                    value: oBatch.description,
+                                },
+                                {
+                                    label: 'Datum',
+                                    value: new Date(oBatch.timestamp).toISOString().split('T')[0],
+                                },
+                                {
+                                    label: 'Material Count',
+                                    value: oBatch.availableMaterialCount,
+                                },
+                            ],
+                        });
+                        // iterate over all Material nodes and request detail information from REST API
+                        oBatch.materials.forEach(oMaterial => {
+                            $.ajax({
+                                url: 'http://localhost:3000/api/Material/' + oMaterial.split('#').pop(),
+                                dataType: 'JSON',
+                                async: false,
+                            }).done(function(oMaterial) {
+                                // transform requested data to fit UI5 control model
+                                aNodesArray.push({
+                                    key: oMaterial.materialId,
+                                    title:
+                                        'Material ' +
+                                        oMaterial.materialId
+                                            .split('#')
+                                            .pop()
+                                            .split('_')
+                                            .pop(),
+                                    icon: 'sap-icon://product',
+                                    status: Number(oBatch.batchId) < 3 ? 'Error' : 'Success',
+                                    group: Number(oBatch.batchId) < 3 ? 'F' : 'S',
+                                });
+                                // add line between Material and corresponding Batch
+                                aLinesArray.push({
+                                    from: oMaterial.materialId,
+                                    to: oBatch.batchId,
+                                });
+                            });
+                        });
+                        if (oBatch.batchId <= 3) {
+                            aLinesArray.push({
+                                from: oBatch.batchId,
+                                to: '3',
+                            });
+                        }
+                        if (oBatch.batchId > 3) {
+                            aLinesArray.push({
+                                from: '3',
+                                to: oBatch.batchId,
+                            });
+                        }
+                    });
+                });
+            });
 
-          this.getView().setModel(oModel);
+            oModel.setProperty('/nodes', aNodesArray);
+            oModel.setProperty('/lines', aLinesArray);
+            oModel.setProperty('/groups', [
+                {
+                    key: 'F',
+                    title: 'Farmer',
+                    icon: 'sap-icon://factory',
+                },
+                {
+                    key: 'S',
+                    title: 'Slaughterhouse',
+                    icon: 'sap-icon://building',
+                },
+                {
+                    key: 'C',
+                    title: 'Cutting Plant',
+                    icon: 'sap-icon://building',
+                },
+            ]);
+            oModel.setProperty('/nodeBoxWidth', 200);
+            this.getView().setModel(oModel);
 
-          this._oModelSettings = new JSONModel({
-            source: "atomicCircle",
-            orientation: "LeftRight",
-            arrowPosition: "End",
-            arrowOrientation: "ParentOf",
-            nodeSpacing: 55,
-            mergeEdges: false
-          });
+            this._oModelSettings = new JSONModel({
+                source: 'atomicCircle',
+                orientation: 'LeftRight',
+                arrowPosition: 'End',
+                arrowOrientation: 'ParentOf',
+                nodeSpacing: 55,
+                mergeEdges: true,
+            });
 
-          this.getView().setModel(this._oModelSettings, "settings");
-
-          var fnSetContent = function(oNode) {
-            oNode.setContent(
-              new ComparisonMicroChart({
-                size: "M",
-                scale: "M",
-                data: [
-                  new ComparisonMicroChartData({
-                    title: "USA",
-                    value: Math.floor(Math.random() * 60),
-                    color: "Neutral"
-                  }),
-                  new ComparisonMicroChartData({
-                    title: "EMEA",
-                    value: Math.floor(Math.random() * 60),
-                    color: "Error"
-                  }),
-                  new ComparisonMicroChartData({
-                    title: "APAC",
-                    value: -20,
-                    color: "Good"
-                  }),
-                  new ComparisonMicroChartData({
-                    title: "LTA",
-                    value: Math.floor(Math.random() * 60) * -1,
-                    color: "Critical"
-                  }),
-                  new ComparisonMicroChartData({
-                    title: "ALPS",
-                    value: Math.floor(Math.random() * 20),
-                    color: "Good"
-                  })
-                ]
-              }).addStyleClass("sapUiSmallMargin")
-            );
-          };
-
-          oModel.attachRequestCompleted(function(oData) {
-            that
-              .byId("graph")
-              .getNodes()
-              .forEach(function(oNode) {
-                if (oNode.getKey() === "21" || oNode.getKey() === "18") {
-                  fnSetContent(oNode);
-                }
-              });
-          });
+            this.getView().setModel(this._oModelSettings, 'settings');
         },
+
         onAfterRendering: function() {
-          this.byId("graphWrapper")
-            .$()
-            .css("overflow-y", "auto");
+            this.byId('graphWrapper')
+                .$()
+                .css('overflow-y', 'auto');
         },
+
         mergeChanged: function(oEvent) {
-          this._oModelSettings.setProperty(
-            "/mergeEdges",
-            !!Number(oEvent.getSource().getProperty("selectedKey"))
-          );
+            this._oModelSettings.setProperty('/mergeEdges', !!Number(oEvent.getSource().getProperty('selectedKey')));
         },
+
         spacingChanged: function(oEvent) {
-          this._oModelSettings.setProperty(
-            "/nodeSpacing",
-            Number(oEvent.getSource().getProperty("selectedKey"))
-          );
-        }
-      }
-    );
-  }
-);
+            this._oModelSettings.setProperty('/nodeSpacing', Number(oEvent.getSource().getProperty('selectedKey')));
+        },
+    });
+});
